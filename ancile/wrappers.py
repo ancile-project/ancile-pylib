96eb129cc2a1e35b832781507d96410171be1ac4
Mohamad Safadieh <me@msafadieh.com>
2019-06-24 10:55:55 -0400

    ancile_function implementation

--- ancile/wrappers.py
+++ ancile/wrappers.py
@@ -1,5 +1,5 @@
 from inspect import getsource
-from re import sub
+import re
 
 '''
     Generates ancile program
@@ -20,7 +20,7 @@ def ancile_function(func):
     code = code.replace("\n    ", "\n")
     parsed_args = [arg.strip() for arg in code[code.index("(") + 1:code.index(")")].split(",")]
     
-    in_args, in_kwargs. in_kwargs_index = [], {}, {}
+    in_args, in_kwargs, in_kwargs_index = [], {}, {}
     for index, parsed_arg in enumerate(parsed_args):
         if not parsed_arg.startswith("*"):
             if "=" in parsed_arg:
@@ -28,12 +28,14 @@ def ancile_function(func):
                 in_kwargs[key] = value
                 in_kwargs_index[index] = key
             else:
-                in_args.add[parsed_arg]
-
-    mul_kwargs = parsed_args[-1].startswith("**")
-    mul_kwargs_kw = parsed_args[-1] if mul_kwargs else None
-    mul_args = (parsed_args[-1].startswith("*") ^ mul_kwargs) or parsed_args[-2].startswith("*")
-    mul_kwargs_kw = parsed_args[:-(1+mul_kwargs)] if mul_args else None
+                in_args.append(parsed_arg)
+    if len(parsed_args) > 0:
+        mul_kwargs = parsed_args[-1].startswith("**")
+        mul_kwargs_kw = parsed_args[-1] if mul_kwargs else None
+        mul_args = (parsed_args[-1].startswith("*") ^ mul_kwargs) or (len(parsed_args) > 1 and parsed_args[-2].startswith("*"))
+        mul_kwargs_kw = parsed_args[:-(1+mul_kwargs)] if mul_args else None
+    else:
+        mul_kwargs = mul_args = False
 
     for _ in range(2):
         code = code[code.index("\n") + 1:]
@@ -49,7 +51,7 @@ def ancile_function(func):
             for arg, supposed_arg in zip(args, in_args):
                 true_args[supposed_arg] = arg
             
-            for arg in in_args[len(args):]
+            for arg in in_args[len(args):]:
                 if arg not in kwargs:
                     raise ValueError
                 else:
@@ -77,7 +79,21 @@ def ancile_function(func):
                     true_args[keyword_arg] = in_kwargs[keyword_arg]
                 else:
                     true_args[keyword_arg] = kwargs.pop(keyword_arg)
-        
+
         ## use string substitution to generate an ancile program
         ## using the function parameters
-        
+
+        var_regex = r"([^\w])(" + "|".join(true_args) + r")([^\w])"
+        repl_function = regex_repl_function(true_args)
+
+        final_code = re.sub(var_regex, repl_function, code)
+        return final_code
+    return final_function
+
+def regex_repl_function(true_args):
+    
+    def repl_function(match_object):
+        before, arg, after = match_object.groups()
+        return before + str(true_args[arg]) + after
+    
+    return repl_function
